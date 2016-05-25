c9d3d8e6e268ce415e110e0295dede4367bab4ea
tfolly <tfolly@student.42.fr>
2016-05-25 21:00:08 +0200

    apparemment c normal que ca se deforme sur les cotes. Cpdt revoir l'eq du plan

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <tfolly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/23 19:36:12 by tfolly            #+#    #+#             */
-/*   Updated: 2016/05/25 20:22:48 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/05/25 20:54:49 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		main(void)
 	int size_line = SIZE * bpp / 8;
 	int end = 1;
 	int		obj_size;
-	t_objs	*sphere;
+	t_objs	*objs;
 	t_data	*data;
 	t_vec	pos;
 
@@ -36,19 +36,21 @@ int		main(void)
 	img = mlx_new_image(mlx, SIZE, SIZE);
 	datastr = mlx_get_data_addr(img, &bpp, &size_line, &end);
 	data = data_init(mlx, img, datastr, bpp, end);
-	data->cam = vec_init(SIZE / 2, SIZE / 2, -SIZE / 2);
+	data->cam = vec_init(SIZE / 2, SIZE / 2, -SIZE / 2); //Il faudrait que je trouve une vrai eq pour la profondeur de la cam
 	// creation d'une liste d'objets
 	pos = vec_init(SIZE / 2, SIZE / 2, SIZE);
 	obj_size = 300;
-	sphere = add_objs(0xFF0000, SPHERE, obj_size, pos, pos, 0);
-	pos = vec_init(SIZE / 4, SIZE / 4, SIZE / 4);
+	objs = add_objs(0xFF0000, SPHERE, obj_size, pos, pos, 0);
+
+	pos = vec_init(0, 0, SIZE);
 	obj_size = 100;
-	sphere = add_objs(0x00FF00, SPHERE, obj_size, pos, pos, sphere);
+	objs = add_objs(0x00FF00, SPHERE, obj_size, pos, pos, objs);
+
 	pos = vec_init(4 * SIZE / 5 , 4 * SIZE / 5, SIZE);
 	obj_size = 200;
-	sphere = add_objs(0x0000FF, SPHERE, obj_size, pos, pos, sphere);
+	objs = add_objs(0x0000FF, SPHERE, obj_size, pos, pos, objs);
 	// remplissage de data
-	calc_plan(datastr, sphere, data);
+	calc_plan(datastr, objs, data);
 	// calc_plan(datastr, sphere->next, data);
 	mlx_put_image_to_window(mlx, win, img, 0, 0);
 	mlx_key_hook(win, my_key_funct, 0);
